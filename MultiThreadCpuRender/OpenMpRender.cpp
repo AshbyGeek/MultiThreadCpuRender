b974201c37e0e428d0fc6dccbe261d6d1284aa4e
ashbygeek <ashbygeek@gmail.com>
2018-03-09 08:17:46 -0500

    Fixed OpenMP NumThreads bug, added improved rendering method based on original line-centric rather than pixel-centric algorithm.

--- MultiThreadCpuRender/OpenMpRender.cpp
+++ MultiThreadCpuRender/OpenMpRender.cpp
@@ -7,24 +7,93 @@
 
 void OpenMpRender::RenderImage(Image* image, Pixel color, std::vector<Line>* lines)
 {
-#pragma omp parallel for
-	for (int x = 0; x < image->width; x++)
+	int x,y;
+
+	#pragma omp parallel for shared(image, color, lines) \
+							 private(x,y) \
+							 schedule(static)
+	for (int i = 0; i < image->width * image->height; i++)
 	{
-		for (int y = 0; y < image->height; y++)
-		{
-			Image::RenderPixel(image, color, lines, x, y);
-		}
+		x = i / image->height;
+		y = i % image->height;
+		Image::RenderPixel(image, color, lines, x, y);
 	}
 }
 
-void OpenMpRender::RenderImageSingleThread(Image*image, Pixel color, std::vector<Line>* lines)
+
+void OpenMpRender::RenderImage2(Image* image, Pixel color, std::vector<Line>* lines)
 {
-	for (int x = 0; x < image->width; x++)
+	for (int i = 0; i < lines->size(); i++)
 	{
-		for (int y = 0; y < image->height; y++)
+		auto line = lines->at(i);
+
+		int tmpdx = line.end.x - line.start.x;
+		int tmpdy = line.end.y - line.start.y;
+
+		if (abs((long)tmpdx) < abs((long)tmpdy))
 		{
-			Image::RenderPixel(image, color, lines, x, y);
+			DrawLineXCentric(image, line, color);
+		}
+		else
+		{
+			DrawLineYCentric(image, line, color);
 		}
 	}
 }
 
+void OpenMpRender::DrawLineXCentric(Image* image, Line line, Pixel color)
+{
+	Point start = line.start;
+	Point end = line.end;
+	if (line.start.y > line.end.y)
+	{
+		start = line.end;
+		end = line.start;
+	}
+
+	int dx = end.x - start.x;
+	int dy = end.y - start.y;
+
+	float x = (float)start.x;
+
+	#pragma omp parallel for
+	for (int y = start.y; y < end.y; y++)
+	{
+		DrawPixelsAt(image, line, color, ceil(x), y);
+		DrawPixelsAt(image, line, color, floor(x), y);
+		x = start.x + dx / (float)dy * (y - start.y);
+	}
+}
+
+void OpenMpRender::DrawLineYCentric(Image* image, Line line, Pixel color)
+{
+	Point start = line.start;
+	Point end = line.end;
+	if (line.start.x > line.end.x)
+	{
+		start = line.end;
+		end = line.start;
+	}
+
+	int dx = end.x - start.x;
+	int dy = end.y - start.y;
+
+	#pragma omp parallel for
+	for (int x = start.x; x < end.x; x++)
+	{
+		float y = start.y + dy / (float)dx * (x - start.x);
+		DrawPixelsAt(image, line, color, x, ceil(y));
+		DrawPixelsAt(image, line, color, x, floor(y));
+	}
+}
+
+void OpenMpRender::DrawPixelsAt(Image* image, Line line, Pixel color, int x, int y)
+{
+	int opacity = Image::RenderPixel(image, line, x, y);
+	if (opacity > 0)
+	{
+		color.a = opacity;
+	}
+	auto originalColor = image->GetPixel(x, y);
+	*originalColor = Image::BlendPixels(*originalColor, color);
+}
--- MultiThreadCpuRender/OpenMpRender.h
+++ MultiThreadCpuRender/OpenMpRender.h
@@ -4,7 +4,9 @@
 namespace OpenMpRender
 {
 	void RenderImage(Image* image, Pixel color, std::vector<Line>* lines);
-
-	void RenderImageSingleThread(Image*image, Pixel color, std::vector<Line>* lines);
+	void RenderImage2(Image * image, Pixel color, std::vector<Line>* lines);
+	void DrawLineXCentric(Image * image, Line line, Pixel color);
+	void DrawLineYCentric(Image * image, Line line, Pixel color);
+	void DrawPixelsAt(Image * image, Line line, Pixel color, int x, int y);
 };
 
